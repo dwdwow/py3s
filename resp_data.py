6f09a05fbf085a0d780b2e9ca68c9f829ab94d06
dwd <wenyi173@gmail.com>
2025-01-09 16:28:36 +0800

    add file

---
+++ resp_data.py
@@ -0,0 +1,12 @@
+from typing import TypeVar, TypedDict, List
+
+D = TypeVar("D")
+
+RespData = TypedDict("RespData",{"success": bool,"data": List[D]})
+
+Errors = TypedDict("Errors",{"code": int,"message": str})
+
+RespError = TypedDict("RespError",{"success": bool,"error": Errors})
+
+
+
