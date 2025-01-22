09221eb7a62254badfd64b96e4718fcee492fbe5
dwd <wenyi173@gmail.com>
2025-01-22 12:22:30 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='py3s',
-    version='0.1.0',
+    version='0.1.3',
     packages=['py3s'],
     install_requires=[
         # List your package dependencies here
