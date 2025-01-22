92641958ec11b872a17d40390fdd13213baac1b9
dwd <wenyi173@gmail.com>
2025-01-22 17:27:24 +0800

    update

--- setup.py
+++ setup.py
@@ -7,8 +7,13 @@ for dir_name in ['build', 'dist']:
     if os.path.exists(dir_name):
         shutil.rmtree(dir_name)
 
-with open('version.txt', 'r') as f:
-    version = f.read().strip("v\n\r\t ")
+# Get version from environment variable if available (for CI/CD)
+version = os.getenv('PYPI_MODULE_PY3S_VERSION')
+if not version:
+    print("Error: No version found in environment variable.")
+    exit(1)
+    
+version = version.strip("v\n\r\t ")
 
 setup(
     name='py3s',
